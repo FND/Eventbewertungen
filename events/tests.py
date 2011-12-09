8988ad22997f74631416c64d97445d75606d16b4
Hendrik Hagmans <hendrik.hagmans@web.de>
2011-12-09 13:42:40 +0100

    Results reworked

--- events/tests.py
+++ events/tests.py
@@ -4,8 +4,10 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
+import datetime
+from django.utils import unittest
 from django.test import TestCase
+from events.models import Event
 
 
 class SimpleTest(TestCase):
@@ -14,3 +16,22 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class EventTestCase(unittest.TestCase):
+    #fixtures = ['eventbewertung.json']
+    
+    def setUp(self):
+        self.event1 = Event.objects.create(name="Event1", pub_date=datetime.datetime.now())
+        self.event2 = Event.objects.create(name="Event2", pub_date=datetime.datetime.now())
+
+    def test(self):
+        self.assertEqual(self.event1.name, 'Event1')
+        self.assertEqual(self.event2.name, 'Event2')
+        self.assert_(self.event1.was_published_today)
+        self.assertTrue(self.event2.was_published_today)
+
+    def testfixtures(self):
+        testevent = Event.objects.get(id=1)
+        self.assert_(testevent != None)
+        self.assert_(testevent.name == "Event1")
--- events/views.py
+++ events/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template.context import RequestContext
 from events.models import Event
+from talks.models import Talk
 import datetime
 from django.http import HttpResponse
 
@@ -18,8 +19,9 @@ def talk(request):
 
 def results(request, event_id):
     p = get_object_or_404(Event, pk=event_id)
-
-
+    
+    #q = get_object_or_404(Talk, pk=talk_id)
+    # in render_to_response: {'talk' : q},
 
     return render_to_response('events/results.html', {'event': p},
                                context_instance=RequestContext(request))
